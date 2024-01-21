a71a82bfe875527c5518b06fa7fb06e1d2bcc134
Kaustubh Patil <157100227+PatilKaustubhShreekrishna@users.noreply.github.com>
2024-01-21 17:39:17 +0530

    Code Added

---
+++ Factorial_number.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int factorial(int);
+
+int main()
+{
+    int num, result;
+    puts("Enter number =");
+    scanf("%d" , &num);
+    
+    result=factorial(num);
+
+    if(result > 0)
+        printf("\nThe Factorial Of %d is = %d\n", num ,result);
+    if(result == -1)
+    
+        puts("\nInvalid Value for finding Factorial\n");
+    
+    
+    return 0;
+}
+
+int factorial(int num)
+{
+    if(num == 0)
+        return 1;
+    else if(num >0)
+        return (num * factorial(num-1));
+    else
+        return -1;
+}
